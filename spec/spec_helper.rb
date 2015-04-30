b6bd1567036dd280933d33db387273982ca5e130
Mike Whitmarsh <mike.whitmarsh@hp.com>
2015-04-30 15:16:00 -0700

    trivial change to test auto-CI

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 require "codeclimate-test-reporter"
 CodeClimate::TestReporter.start
 
-require 'live_paper' # and any other gems you need
+require 'live_paper'
 
 # include helpers
 Dir[File.expand_path(File.join(File.dirname(__FILE__), 'spec_helpers', '**', '*.rb'))].each { |f| require f }
