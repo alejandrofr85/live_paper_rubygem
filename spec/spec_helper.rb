85b35dbd53d75013972d87baf5e23b0d798ead81
Mike Whitmarsh <mike.whitmarsh@hp.com>
2015-04-30 15:13:48 -0700

    added code climate test coverage [#93462438]

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,3 +7,5 @@ require 'live_paper' # and any other gems you need
 Dir[File.expand_path(File.join(File.dirname(__FILE__), 'spec_helpers', '**', '*.rb'))].each { |f| require f }
 
 require 'webmock/rspec'
+
+WebMock.disable_net_connect!(:allow => 'codeclimate.com')
