6f975630abc9efe0f990c9ff8db24f62b2cf82e1
Live Paper <livepaper@Stevens-Old-seat-iMac.local>
2015-10-13 09:55:57 -0700

    [#105490506] Add adjustImageLevels option to WmTrigger.download_watermark method.

--- lib/live_paper/wm_trigger.rb
+++ lib/live_paper/wm_trigger.rb
@@ -5,6 +5,7 @@ module LivePaper
     attr_accessor :wm_url
     WATERMARK_RESOLUTION = 75
     WATERMARK_STRENGTH = 10
+    ADJUST_IMAGE_LEVELS = false
 
     def parse(data)
       data = JSON.parse(data, symbolize_names: true)[:trigger]
@@ -16,7 +17,9 @@ module LivePaper
     def download_watermark(image_url, options = {})
       resolution = options[:resolution] || WATERMARK_RESOLUTION
       strength = options[:strength] || WATERMARK_STRENGTH
-      url = "#{self.wm_url}?imageUrl=#{CGI.escape(image_url)}&resolution=#{resolution}&strength=#{strength}"
+      adjustImageLevels = (options[:adjustImageLevels].nil? ? ADJUST_IMAGE_LEVELS : options[:adjustImageLevels])
+      url = "#{self.wm_url}?imageUrl=#{CGI.escape(image_url)}&resolution=#{resolution}&strength=#{strength}&adjustImageLevels=#{adjustImageLevels}"
+      puts "URL=#{url}" if (!options[:showUrl].nil?)
       begin
         response = WmTrigger.rest_request( url, :get, accept: "image/jpg" )
         response.body.empty? ? nil : response.body
